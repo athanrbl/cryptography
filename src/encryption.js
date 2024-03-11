2170ed5cc5f76b922216d25a519d54b272821b0a
athanrbl <148010540+athanrbl@users.noreply.github.com>
2024-03-11 16:59:02 +0200

    Polybius Prototype logic
    Experimenting with the polybius cipher using lists and indexes

--- src/encryption.js
+++ src/encryption.js
@@ -1,5 +1,31 @@
+// resources
+// cipher 1
+let l1 = ['a','b','c','d','e']
+let l2 = ['f','g','h','i','j']
+let l3 = ['k','l','m','n','o']
+let l4 = ['p','q','r','s','t']
+let l5 = ['u','v','w','x','y'] // missing z
+let arrayList = [l1,l2,l3,l4,l5]
+let letterArray = []
+let output = ""
+
+// ---------------
+
 function Encrypt1() {
-    return
+
+    let input = prompt('Encrypt a word: ') // input 'hey' : array = ['h','e','y']
+    letterArray = input.split('') // split by each character
+    letterArray.forEach(letter => {
+
+        arrayList.forEach(numList => {
+            if(numList.indexOf(letter) != -1) {
+                output += letter // should return input
+            } else {return}
+        });
+    })
+    console.log('Your result is: ' + output)
+
+    // lists = x, list index = y
 }
 
 

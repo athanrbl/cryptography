32b9fd01539a04cbfaf0ffb2558c0033ebac83ac
vnwoke <120103154+Vnwoke@users.noreply.github.com>
2024-03-19 20:55:45 +0200

    ceaser cipher (encryption2)

---
+++ src/decryption.js
@@ -0,0 +1,13 @@
+function Decrypt1() {
+    return
+}
+
+
+function Decrypt2(text, shift) {
+	return
+}
+
+
+function Decrypt3() {
+    return
+}
--- src/encryption.js
+++ src/encryption.js
@@ -3,11 +3,31 @@ function Encrypt1() {
 }
 
 
-function Encrypt2() {
-    return
+function Encrypt2(text, shift) {
+    //Ceaser Cipher
+	
+	// Encrypts text using a shift 
+	let result=""
+	for (let i = 0; i < text.length; i++)
+	{
+		let char = text[i];
+		if (char.toUpperCase(text[i]))
+		{
+			let ch = String.fromCharCode((char.charCodeAt(0) + shift-65) % 26 + 65);
+			result += ch;
+		}
+		else
+		{
+			let ch = String.fromCharCode((char.charCodeAt(0) + shift-97) % 26 + 97);
+			result += ch;
+		}
+	}
+	return result;
 }
 
 
-function Encrypt3() {
-    return
+
+
+function Encrypt3(){
+	return
 }

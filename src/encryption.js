6783053729b0bfa6dd6088fe4dd61672cd112412
athanrbl <148010540+athanrbl@users.noreply.github.com>
2024-05-17 09:24:34 +0300

    encrypt/decrypt load correctly

--- src/encryption.js
+++ src/encryption.js
@@ -1,39 +1,34 @@
 // resources
 // cipher 1
-let l1 = ['a','b','c','d','e']
-let l2 = ['f','g','h','i','j']
-let l3 = ['k','l','m','n','o']
-let l4 = ['p','q','r','s','t']
-let l5 = ['u','v','w','x','y'] // missing z
-let arrayList = [l1,l2,l3,l4,l5]
+const l1 = ['a','b','c','d','e']
+const l2 = ['f','g','h','i','j']
+const l3 = ['k','l','m','n','o']
+const l4 = ['p','q','r','s','t']
+const l5 = ['u','v','w','x','y']
+const l6 = ['z',' ',',','?','\'']
+const arrayList = [l1,l2,l3,l4,l5,l6]
+
+// used for encoding
 let letterArray = []
 let output = ""
 
 // ---------------
 
-/*export default*/ function Encrypt1() {
+function Encrypt1() {
 
-    let input = document.getElementById("InputT").textContent // input 'hey' : array = ['h','e','y']
-    console.log(input)
+    let input = document.getElementById("InputT").value // input 'hey' : array = ['h','e','y']
     letterArray = input.split('') // split by each character
-    letterArray.forEach(letter => {
 
-        arrayList.forEach(numList => {
-            if(numList.indexOf(letter) != -1) {
-                output += letter // should return input
-            } else {return}
+    letterArray.forEach(character => {
+
+        arrayList.forEach((listNumbered, i) => {
+          //console.log(letterArray, character, listNumbered, listNumbered.indexOf(character))
+          if(listNumbered.indexOf(character) != -1) { // if character is found (-1 is not found)
+            output += i+1 // should return list number
+            output += listNumbered.indexOf(character)+1 // should return character index in list
+          } else {return}
         });
     })
-    console.log('Your result is: ' + output)
-    // lists = x, list index = y
+    document.getElementById("OutputT").value = output //returns output
+    output = ""
 }
-
-/*
-function Encrypt2() {
-    return
-}
-
-
-function Encrypt3() {
-    return
-}*/
